2782e523a68c5795e284e932200a52e44f3ff269
Dmytro Yarmak <dmytroyarmak@gmail.com>
2014-11-01 19:39:45 +0200

    Add real seeds with images

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141101143830) do
+ActiveRecord::Schema.define(version: 20141101172347) do
 
   create_table "products", force: true do |t|
     t.string   "title"
     t.text     "description"
-    t.decimal  "price",             precision: 10, scale: 0
+    t.decimal  "price",              precision: 10, scale: 0
     t.integer  "delivery_from"
     t.integer  "delivery_to"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "short_description"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-require 'json'
+require "json"
 
-file = File.read('db/products.seeds.json')
-products_json = JSON.parse(file)
+products_json = JSON.parse(File.read("db/seeds/products.json"))
 
-products_json.each do |product|
-  Product.create(product)
+products_json.each do |product_data|
+  Product.create!(product_data.merge({
+    image: File.open("db/seeds/product_images/#{product_data["image"]}")
+  }))
 end
